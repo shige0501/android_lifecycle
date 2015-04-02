963e5da8795109ff2d2b52959926749797e636be
Koji Shigemura <shige0501@gmail.com>
2015-04-02 11:20:38 +0900

    Logを追加

--- app/src/main/java/net/buildbox/test/android_lifecycle/MainActivity.java
+++ app/src/main/java/net/buildbox/test/android_lifecycle/MainActivity.java
@@ -2,18 +2,63 @@ package net.buildbox.test.android_lifecycle;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "call onCreate() method.");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "call onStart() method.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "call onResume() method.");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.d(TAG, "call onRestart() method.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "call onPause() method.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "call onStop() method.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "call onDestroy() method.");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
